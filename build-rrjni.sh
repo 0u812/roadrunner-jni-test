1f786cf62f3af640a8667ad46b488652c352ee83
0u812 <0u812@github.com>
2014-11-16 20:40:00 -0800

    build roadrunner

--- build-rrjni.sh
+++ build-rrjni.sh
@@ -9,4 +9,12 @@ mkdir -p build/rr-thirdparty && cd build/rr-thirdparty
 
 cmake -DCMAKE_INSTALL_PREFIX=../../install/rr-thirdparty ../../roadrunner/third_party && make -j4 install
 
+# make rr build dir
+mkdir ../roadrunner && cd ../roadrunner
+
+cmake -DCMAKE_INSTALL_PREFIX=../../install/roadrunner -DTHIRD_PARTY_INSTALL_FOLDER=../../install/rr-thirdparty -DCMAKE_BUILD_TYPE=Release -DBUILD_PYTHON=OFF -DBUILD_TESTS=OFF -DBUILD_TEST_TOOLS=OFF ../../roadrunner
+
+# return to root path
+cd ../..
+
 $CXX -I$JDK_INCLUDE proj.cpp TestClass.cpp -I$JNI_INCLUDE -I$RR_INCLUDE -I$RR_TP_INCLUDE -L$RR_LIB_PATH -lroadrunner -shared -fPIC -o libproj.so
