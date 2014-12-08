3a45e50a4eb6061e01d4ece28ac766cf7658004c
0u812 <0u812@github.com>
2014-12-08 12:53:07 -0800

    build java code

--- build-rrjni.sh
+++ build-rrjni.sh
@@ -15,15 +15,18 @@ if [ ! -f ./user-config.sh ]; then
   exit 1
 fi
 
+# Get config vars
 source ./user-config.sh
 
 mkdir -p build/rr-thirdparty && cd build/rr-thirdparty
 
+# Build rr deps
 cmake -DCMAKE_INSTALL_PREFIX=../../install/rr-thirdparty ../../roadrunner/third_party && make -j4 install
 
 # make rr build dir
 mkdir -p ../roadrunner && cd ../roadrunner
 
+# Build roadrunner
 cmake -DCMAKE_INSTALL_PREFIX=../../install/roadrunner -DTHIRD_PARTY_INSTALL_FOLDER=../../install/rr-thirdparty -DCMAKE_BUILD_TYPE=Release -DBUILD_PYTHON=OFF -DBUILD_TESTS=OFF -DBUILD_TEST_TOOLS=OFF ../../roadrunner && make -j4 install
 
 # return to root path
@@ -35,3 +38,14 @@ RR_TP_INCLUDE=install/rr-thirdparty/include
 RR_LIB_PATH=$RR_INSTALL_ROOT/lib
 
 $CXX -I$JDK_INCLUDE proj.cpp TestClass.cpp -I$JNI_INCLUDE -I$RR_INCLUDE -I$RR_TP_INCLUDE -L$RR_LIB_PATH -lroadrunner -shared -fPIC -o libproj.so
+
+# Export the required library paths
+export LD_LIBRARY_PATH=/vagrant:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/vagrant/install/roadrunner/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/vagrant/install/rr-thirdparty/lib:$LD_LIBRARY_PATH
+
+# Compile Java code
+javac test/myproj/test.jav
+
+# Run Java code
+java test.myproj.testa
